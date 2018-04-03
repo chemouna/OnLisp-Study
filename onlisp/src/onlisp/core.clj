8151bf553c5be75171bb3ea229e265f07723e770
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-03 21:41:52 +0100

    Start

---
+++ onlisp/src/onlisp/core.clj
@@ -0,0 +1,6 @@
+(ns onlisp.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ onlisp/test/onlisp/core_test.clj
@@ -0,0 +1,7 @@
+(ns onlisp.core-test
+  (:require [clojure.test :refer :all]
+            [onlisp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
