e191c846d2e72bae4da3302e62838094aac3816d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-09 23:25:26 +0100

    chapter 4

---
+++ onlisp/src/onlisp/chapter4.clj
@@ -0,0 +1,12 @@
+
+(ns onlisp.chapter4)
+
+(defn find2
+  [f lst]
+  (when lst
+    (let [val (f (first lst))]
+      (if val
+        [(first lst) val]
+        (find2 f (rest lst))))))
+
+;(find2 even? '(1 2 3))
