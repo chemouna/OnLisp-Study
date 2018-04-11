6b2961a17aae2978e5b79128b21651f3d3edacae
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-11 10:19:34 +0100

    Continue onlisp

--- onlisp/src/onlisp/chapter4.clj
+++ onlisp/src/onlisp/chapter4.clj
@@ -1,5 +1,7 @@
+(ns onlisp.chapter4
+  (:use [clojure.tools.trace :as trace]))
 
-(ns onlisp.chapter4)
+(trace/trace-ns 'onlisp.chapter4)
 
 (defn find2
   [f lst]
@@ -10,3 +12,50 @@
         (find2 f (rest lst))))))
 
 ;(find2 even? '(1 2 3))
+
+(defn last1
+  [s]
+  (last s))
+
+(defn single
+  [s]
+  (and (seq? s) (not (next seq))))
+
+(defn append1
+  [lst obj]
+  (concat lst (list obj)))
+
+(defn mklist [obj] (if (sequential? obj) obj (list obj)))
+
+(defn conc1 [sq elem]
+  (dosync
+   (if (seq? elem)
+     (ref-set sq (cons @sq elem))
+     (ref-set sq (concat @sq (vector elem))))))
+
+(defn longer
+  [x y]
+  (if (and (sequential? x) (sequential? y))
+    (loop [x (next x) y (next y)]
+      (and x (or (not y) (recur (next x) (next y)))))
+    (> (count x) (count y))))
+
+(defn filtr [f lst]
+  (for [x lst :when (f x)]
+    x))
+
+;(filtr even? '(2 3 4 5 6 7 8 9))
+
+(defn group [source n]
+  (if (zero? n) (pr "error: zero length"))
+
+  (let [t (int (/ (count source) n))
+        remainder (drop (* n t) source)]
+    (lazy-cat
+     (for [i (range t)]
+       (take n (nthrest source (* i n))))
+     (if (nil? remainder)
+       '()
+       (list remainder)))))
+
+(group '(1 2 3 4 5 6 7) 4)
